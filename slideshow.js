89ea2f766aff56f74d6d12e34133e63f836495c4
JeongMin Lee <76844856+froggy1014@users.noreply.github.com>
2022-04-06 05:30:03 +0900

    slide part done.

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,9 @@
 $('.myslider').slick({
-    infinite:true,
-    dots:true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed:4000,
+    infinite:true,         // 마지막 슬라이드에서 처음으로 돌아오는 기능 
+    //dots:true,             // 슬라이드 표시 점들
+    slidesToShow: 1,       // 박스안에 보이는 슬라이드 개수 
+    slidesToScroll: 1,     // 한번에 움직이는 슬라이드 개수 
+    autoplaySpeed: 1000,
     speed:500,
     fade: true,
     cssEase: 'linear',
@@ -29,3 +28,23 @@ $('.myslider').slick({
         }
       ]
 });
+
+$('.slide-prev').click(function(){ 
+  $('.myslider').slick('slickPrev');
+} );
+
+$('.slide-next').click(function(){
+  $('.myslider').slick('slickNext');
+} );  
+
+$('.auto_play').click(function(){   
+    if ($('.play_toggle').hasClass("fa-play") ===true) {
+     $('.myslider').slick('slickPlay');
+    $('.play_toggle').removeClass('fa-play').addClass('fa-pause');
+    }else{
+        $('.myslider').slick('slickPause');
+       $('.play_toggle').removeClass('fa-pause').addClass('fa-play');
+    } 
+});
+
+
