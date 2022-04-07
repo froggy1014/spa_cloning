b914107260c3c76a82087322061162942328bde5
JeongMin Lee <76844856+froggy1014@users.noreply.github.com>
2022-04-07 20:50:35 +0900

    content_list switch function done.

---
+++ contents.js
@@ -0,0 +1,16 @@
+$('.content_button_shape').click(function(){
+    $(this).addClass('btn_active');
+    $(this).siblings().removeClass('btn_active');
+    if ($(this).hasClass('chobo'))
+    {
+        $('#chobo').addClass('active')
+        $('#chobo').siblings().removeClass('active')
+    }else if($(this).hasClass('data')){
+        $('#data').addClass('active')
+        $('#data').siblings().removeClass('active')
+    }else{
+        $('#dev').addClass('active')
+        $('#dev').siblings().removeClass('active')
+    }
+
+})
