ca6ec2842a935697e91fd8d1f28c5bb0cac850c7
JeongMin Lee <76844856+froggy1014@users.noreply.github.com>
2022-04-14 22:56:20 +0900

    widget done.

--- widget.js
+++ widget.js
@@ -1,16 +1,16 @@
 
 
-document.querySelector(".chat_rtan").addEventListener("click", function(){
-    document.getElementsByClassName("chat_rtan")[0].classList.add("hidden");
-    document.getElementsByClassName("chat_rtan_widget")[0].classList.add("active");
-    document.getElementsByClassName("chat_rtan_widget")[0].classList.add("widget_active");
+document.querySelector("#chat_rtan").addEventListener("click", function(){
+    document.getElementsByClassName("chat_rtan")[0].classList.remove("chat_rtan_active");
+    document.getElementsByClassName("chat_rtan")[0].classList.add("chat_rtan_hide");
+    document.getElementsByClassName("widget_window")[0].classList.add("widget_active");
     console.log("open");
 });
 
 document.querySelector(".widget_close").addEventListener("click",function(){
-    document.getElementsByClassName("chat_rtan")[0].classList.add("active");
-    document.getElementsByClassName("chat_rtan_widget")[0].classList.add("hidden");
-    document.getElementsByClassName("chat_rtan_widget")[0].classList.remove("widget_active");
+    document.getElementsByClassName("chat_rtan")[0].classList.remove("chat_rtan_hide");
+    document.getElementsByClassName("chat_rtan")[0].classList.add("chat_rtan_active");
+    document.getElementsByClassName("widget_window")[0].classList.remove("widget_active");
     console.log("close");
 });
 
